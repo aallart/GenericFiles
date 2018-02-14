dce0e2e1a1e58ef7f6582ffde354bc26f0515313
Alan ALLART <Alan.ALLART@student.isae-supaero.fr>
2018-02-14 05:31:40 +0100

    error on insert corrected and remove ok !

--- double_linked_list.c
+++ double_linked_list.c
@@ -22,7 +22,7 @@ void print_list(const dlist *p_list){
 	cell *p = p_list->first;
 	printf("NULL <--> ");
 	for(i = 0; i < p_list->size; i++) {
-		print_elt(p->data);printf(" <--> ");
+		print_elt(p->data);printf(" <--> ");//printf("%p: ", p->next);
 		p = p->next;}
 	printf("NULL");
 }
@@ -32,10 +32,10 @@ void cons(dlist *p_list, elt data){
         cell *p_cell = NULL;
         p_cell = malloc(sizeof(cell));
         if (p_cell == NULL) {exit(EXIT_FAILURE);}
-        p_cell->data = data;
+        p_cell->data = data;        //update new cell values
         p_cell->next = p_list->first;
         p_cell->previous = NULL;
-        if (p_list->size > 0)
+        if (p_list->size > 0)       //Only if list wasn't empty
             (p_list->first)->previous = p_cell;
         else
             p_list->last = p_cell;
@@ -49,10 +49,10 @@ void snoc(dlist *p_list, elt data){
         cell *p_cell = NULL;
         p_cell = malloc(sizeof(cell));
         if (p_cell == NULL) {exit(EXIT_FAILURE);}
-        p_cell->data = data;
+        p_cell->data = data;            //update new cell values
         p_cell->previous = p_list->last;
         p_cell->next = NULL;
-        if (p_list->size > 0)
+        if (p_list->size > 0)   //if list wasn't empty
             (p_list->last)->next = p_cell;
         else
             p_list->first = p_cell;
@@ -78,20 +78,46 @@ elt get_element(const dlist *p_list, int pos){
 
 void insert_element(dlist *p_list, int pos, elt data){
     if (pos >= 0 && pos <= p_list->size){
-        if(pos == 0) cons(p_list, data);
-        else if(pos == p_list->size) snoc(p_list, data);
-        else{
+        if(pos == 0) cons(p_list, data);    //if add at the beginning of the list
+        else if(pos == p_list->size) snoc(p_list, data);    //at the end
+        else{                                   //somewhere else
             int i;
-            cell *p = p_list->first, *p_new = NULL;
+            cell *p = p_list->first, *p_new = NULL, *p_temp = NULL;
             p_new = malloc(sizeof(*p_new));
             if(p_new == NULL) exit(EXIT_FAILURE);
             p_new->data = data;
             for(i = 1; i < pos; i++){p = p->next;}
             p_new->next = p->next;
             p_new->previous = p;
+            p_temp = p->next;
             p->next = p_new;
-            p->next->previous = p_new;
+            p_temp->previous = p_new;
             p_list->size += 1;
         }
+    }
+}
+
+void remove_element(dlist *p_list, int pos){
+    cell *p = NULL;
+    if(pos >= 0 && pos < p_list->size){
+        if(pos == 0){       //remove at the beginning
+            p = p_list->first;
+            p_list->first = p->next;
+            p->next->previous = NULL;
+        }
+        else if (pos == (p_list->size - 1)){  //at the end
+            p = p_list->last;
+            p_list->last = p->previous;
+            p->previous->next = NULL;
+        }
+        else{       //somewhere in the middle
+            int i;
+            p = p_list->first;
+            for(i = 1; i <= pos; i++){p = p->next;}
+            p->previous->next = p->next;
+            p->next->previous = p->previous;
+        }
+        free(p);
+        p_list->size--;
     }
 }
--- main.c
+++ main.c
@@ -27,7 +27,10 @@ int main(void) {
     print_list(p_list);printf(" => empty = %s", (is_empty(p_list))?"yes":"no");
     for(i = 0; i < 4; i++) {printf("\n%dth element: ", i); print_elt(get_element(p_list, i));}
     printf("\n");
-    insert_element(p_list, 5, elements[4]);print_list(p_list);
+    insert_element(p_list, 3, elements[4]);print_list(p_list);
+    printf("\n");
+    //print_elt(p_list->last->previous->next->previous->data);
+    remove_element(p_list, 0);print_list(p_list);
 
     return 0;
 }
